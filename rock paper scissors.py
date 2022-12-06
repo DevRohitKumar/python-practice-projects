9af620df9d80fb0b527fd76eaf471d5ba06e2b9e
Rohit Kumar <88289975+SigmaQuasar@users.noreply.github.com>
2022-12-06 16:16:46 +0530

    simplest classic rock paper scissors created

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,3 +1,61 @@
+#welcome to classic 🪨📰✂️ 
 
+import random
+import os
+import time
 
- 
+rps_choices = ["rock", "paper", "scissors"]
+
+# def clear():
+#     os.system('cls' if os.name == 'nt' else 'clear')
+    
+# def sleep_timer(t):
+#     time.sleep(t)
+
+while True:
+    comp_choice = random.choice(rps_choices)
+    user_choice = input('Enter "Rock" or "Paper" or "Scissors" to choose: ')
+    user_choice_lower = user_choice.lower()
+    
+    def print_selection():
+        print("You chose: "+user_choice.capitalize())
+        print("💻 chose: "+comp_choice.capitalize())
+        print()
+    
+    def user_win():
+        salut_options = ["Congrats", "Congratulations", "Yippee"]
+        print(random.choice(salut_options)+"! You win! 🥳 ")
+        
+    def com_win():
+        salut_options = ["Oops", "Sorry", "Hard Luck"]
+        print(random.choice(salut_options)+"! 💻 win! 😥 ")
+        
+    
+    if user_choice_lower == comp_choice:
+        print_selection()
+        print("Game ties!")
+        
+    elif user_choice_lower == "rock":
+        if comp_choice == "paper":
+            print_selection()
+            com_win()
+        elif comp_choice == "scissors":
+            print_selection()
+            user_win()
+            
+    elif user_choice_lower == "paper":
+        if comp_choice == "rock":
+            print_selection()
+            user_win()
+        elif comp_choice == "scissors":
+            print_selection()
+            com_win()
+            
+    elif user_choice_lower == "scissors":
+        if comp_choice == "rock":
+            print_selection()
+            com_win()
+        elif comp_choice == "paper":
+            print_selection()
+            user_win()
+            
